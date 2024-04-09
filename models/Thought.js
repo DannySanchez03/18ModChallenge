7cc4b0b1137dba051c949a6fe3db5e25a1670411
DannySanchez03 <dannyproa303@gmail.com>
2024-04-09 04:00:20 -0500

    models finished

---
+++ models/Thought.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+// Thought schema
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: [reactionSchema]
+},
+{
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+});
+
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
